00cc7beffad9b202ee66d8f228e7ab28152552cc
LewinGerber <57451950+LewinGerber@users.noreply.github.com>
2020-10-03 21:30:05 +0200

    Half Sword: Updated functionality

--- MWM/src/main/java/ch/hyperreal/nawm/objects/items/tools/HalfSword.java
+++ MWM/src/main/java/ch/hyperreal/nawm/objects/items/tools/HalfSword.java
@@ -1,24 +1,32 @@
 package ch.hyperreal.nawm.objects.items.tools;
 
 import ch.hyperreal.nawm.MinecraftWeaponsMod;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.material.Material;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.*;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.HandSide;
 import net.minecraft.world.World;
 
 /**
- * @author Simon Tobler
- * @version 26.09.2020
+ * @author Simon Tobler, Lewin Gerber
+ * @version 03.10.2020
  */
 public class HalfSword extends SwordItem {
+    private float destroyStrength;
+    private BlockState temporaryBlockState;
+
     public HalfSword() {
         super(ToolTier.AdventureTier.HALF_SWORD, 0, 0.0F, new Item.Properties().group(MinecraftWeaponsMod.WeaponItemGroup.instance));
+        destroyStrength = 100.0F;
     }
 
     @Override
@@ -31,8 +39,4 @@ public class HalfSword extends SwordItem {
         return super.hitEntity(stack, target, attacker);
     }
 
-    @Override
-    public float getDestroySpeed(ItemStack stack, BlockState state) {
-        return 0.0F;
-    }
 }
